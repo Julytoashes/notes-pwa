75f19fd2a2159499817d2d89504f69aca4059231
Julytoashes <xuelinlcn@gmail.com>
2025-06-12 17:44:45 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,21 @@
-const CACHE_NAME = 'my-pwa-cache-v4';
-
+const CACHE_NAME = 'my-notes-pwa-cache-v5';
 const urlsToCache = [
-
-  '/index.html',
-
-  'https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.1.0/style.css'
-
+    '/index.html',
+    '/manifest.json',
+    'https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.1.0/style.css'
+    // 添加其他资源（如 JavaScript 文件）如果需要
 ];
 
 self.addEventListener('install', event => {
-
-  event.waitUntil(
-
-    caches.open(CACHE_NAME)
-
-      .then(cache => cache.addAll(urlsToCache))
-
-  );
-
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', event => {
-
-  event.respondWith(
-
-    caches.match(event.request)
-
-      .then(response => {
-
-        return response || fetch(event.request);
-
-      })
-
-  );
-
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
